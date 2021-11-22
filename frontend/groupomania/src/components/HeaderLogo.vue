470a50823dc56bd85a373d75c6bb6b022479b9d3
decorne <sebastiendecorne@gmail.com>
2021-11-22 15:59:04 +0100

    Create HeaderLogo.vue

---
+++ frontend/groupomania/src/components/HeaderLogo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <img src="../assets/icon-left-font-monochrome-white" alt="Logo Groupomania">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderLogo'
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+    color: #fd2d01;
+}
+</style>
